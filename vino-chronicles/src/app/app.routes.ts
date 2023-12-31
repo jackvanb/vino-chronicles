154d21dede10f24351097386c6111282bedf2bdb
Jack Van Boening <jackvanboening@gmail.com>
2023-12-31 00:08:20 +0000

    create landing component and update routing

--- vino-chronicles/src/app/app.routes.ts
+++ vino-chronicles/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from '../pages/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LandingComponent,
+  },
+];
---
+++ vino-chronicles/src/pages/landing/landing.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LandingComponent {
+
+}

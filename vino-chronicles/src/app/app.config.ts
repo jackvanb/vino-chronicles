0d982a8a30de76501f59ac6212e24d63a2133d41
Jack Van Boening <jackvanboening@gmail.com>
2024-01-14 22:42:05 +0000

    add firebase/firestore

--- vino-chronicles/src/app/app.config.ts
+++ vino-chronicles/src/app/app.config.ts
@@ -10,9 +10,23 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(StarRatingModule.forRoot()),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'vino-chronicles',
+          appId: '1:942559100849:web:fe7fa71ae3e6ca735b4790',
+          storageBucket: 'vino-chronicles.appspot.com',
+          apiKey: 'AIzaSyCarl13Q-YGIV0fqHBfVN8AfYBGIRp0Q30',
+          authDomain: 'vino-chronicles.firebaseapp.com',
+          messagingSenderId: '942559100849',
+          measurementId: 'G-CGB9T9VPC5',
+        })
+      ),
+      provideFirestore(() => getFirestore()),
+      StarRatingModule.forRoot()
+    ),
     provideRouter(routes),
     provideClientHydration(),
-    provideAnimations()importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"vino-chronicles","appId":"1:942559100849:web:fe7fa71ae3e6ca735b4790","storageBucket":"vino-chronicles.appspot.com","apiKey":"AIzaSyCarl13Q-YGIV0fqHBfVN8AfYBGIRp0Q30","authDomain":"vino-chronicles.firebaseapp.com","messagingSenderId":"942559100849","measurementId":"G-CGB9T9VPC5"})))importProvidersFrom(provideFirestore(() => getFirestore())),
+    provideAnimations(),
   ],
 };

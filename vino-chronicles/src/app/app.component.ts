6ce6073a75a6eeb0dacb192fab9e862b22b5f342
Jack Van Boening <jackvanboening@gmail.com>
2024-03-24 23:14:42 +0000

    add toolbar

--- vino-chronicles/src/app/app.component.ts
+++ vino-chronicles/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, MatToolbarModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'vino-chronicles';
